8d894953482e3c392c2e81e77d97a34cca4ddf87
Dejan <alf_26@mail.de>
2021-08-19 22:19:10 +0200

    init commit

---
+++ todolist_editor/src/main/java/de/dejan/todolist/Main.java
@@ -0,0 +1,33 @@
+package de.dejan.todolist;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main{
+    public static void main( String[] args ){
+        String[] tasks=new String[10];
+
+        System.out.println("ToDo-Liste:");
+
+        System.out.println("\n10 Aufgaben hinzufügen:");
+
+        for(int i=0; i<10; i++){
+
+            tasks[i]=System.console().readLine();
+
+        }
+
+        System.out.println("\nListe:");
+
+        for(int i=0; i<10; i++){
+
+            System.out.println(tasks[i]);
+
+        }
+
+
+    }
+}
