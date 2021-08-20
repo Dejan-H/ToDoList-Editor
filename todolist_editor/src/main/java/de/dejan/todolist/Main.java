6283dff99fe3d225a75ce5bcad31d27b62b61886
Dejan <alf_26@mail.de>
2021-08-21 01:00:45 +0200

    added class ToDo added menu in Main class

--- todolist_editor/src/main/java/de/dejan/todolist/Main.java
+++ todolist_editor/src/main/java/de/dejan/todolist/Main.java
@@ -1,28 +1,58 @@
 package de.dejan.todolist;
 
+import java.util.*;
+
+
 
 public class Main{
+
+    static int taskID=0;
+    static ArrayList<ToDo> tasks=new ArrayList<ToDo>();
+
     public static void main( String[] args ){
-        String[] tasks=new String[10];
-
-        System.out.println("ToDo-Liste:");
-
-        System.out.println("\n10 Aufgaben hinzufügen:");
-
-        for(int i=0; i<10; i++){
-
-            tasks[i]=System.console().readLine();
-
+        
+        while (true) {
+            menu();
         }
-
-        System.out.println("\nListe:");
-
-        for(int i=0; i<10; i++){
-
-            System.out.println(tasks[i]);
-
-        }
+        
 
 
     }
+
+    public static void menu(){
+
+        System.out.println("\n[1]Neues ToDo hinzufügen");
+        System.out.println("[2]ToDos auflisten");
+        System.out.println("[3]Beenden");
+
+        int in=Integer.parseInt(System.console().readLine());
+
+        switch (in) {
+            case 1: newTask(); break;
+            case 2: listToDo(); break;
+            case 3: System.exit(0);
+        }
+
+    }
+
+    public static void newTask(){
+
+        System.out.println("\nAufgabe hinzufügen:");
+
+        String in=System.console().readLine();
+
+        ToDo task=new ToDo(taskID, in);
+        tasks.add(task);
+        taskID++;
+        return;
+    }
+
+    public static void listToDo(){
+        System.out.println("\n\nToDo:");
+        System.out.println("=====");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println("["+tasks.get(i).theId+"] "+tasks.get(i).theContent);
+        }
+    }
+
 }
---
+++ todolist_editor/src/main/java/de/dejan/todolist/ToDo.java
@@ -0,0 +1,15 @@
+package de.dejan.todolist;
+
+public class ToDo {
+    
+    int theId;
+    String theContent;
+
+    public ToDo(int id, String content){
+        theId=id;
+        theContent=content;
+    }
+
+    
+
+}
