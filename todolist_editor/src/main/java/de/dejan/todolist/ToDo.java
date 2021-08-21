b243ab9abb281ba2fac1dc838961407c3898d574
Dejan <alf_26@mail.de>
2021-08-21 22:04:49 +0200

    added attributes to the constructor

--- todolist_editor/src/main/java/de/dejan/todolist/ToDo.java
+++ todolist_editor/src/main/java/de/dejan/todolist/ToDo.java
@@ -4,12 +4,22 @@ public class ToDo {
     
     int theId;
     String theContent;
+    private boolean theDone;
+    String theDeadline;
 
-    public ToDo(int id, String content){
+    public ToDo(int id, String content, boolean done){
         theId=id;
         theContent=content;
+        theDone=done;
     }
 
-    
+    public void setDone(boolean done){
+        theDone=done;
+    }
+
+    public boolean getDone(){
+        return theDone;
+    }
+
 
 }
